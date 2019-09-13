8ec878c8b7773e80fcda2274e4aa5a9945cebf05
crystal98tang <tyk502709661@126.com>
2019-09-13 15:07:06 +0800

    start project

--- main.py
+++ main.py
@@ -1,24 +1,65 @@
-from keras.callbacks import TensorBoard
-
+from tensorflow.python.keras.callbacks import ModelCheckpoint,TensorBoard
 from model import *
 from data_set import *
+import os
 
-#os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+import tensorflow as tf
+from tensorflow.python.keras.utils import multi_gpu_model
+class ParallelModelCheckpoint(ModelCheckpoint):
+    def __init__(self,model,filepath, monitor='loss', verbose=0,
+                 save_best_only=False, save_weights_only=False,
+                 mode='auto', period=1):
+        self.single_model = model
+        super(ParallelModelCheckpoint,self).__init__(filepath, monitor, verbose,save_best_only, save_weights_only,mode, period)
 
+    def set_model(self, model):
+        super(ParallelModelCheckpoint,self).set_model(self.single_model)
 
-data_gen_args = dict(rotation_range=0.2,
+from divideImage import init_box
+
+data_gen_args = dict(rotation_range=2,
                     width_shift_range=0.05,
                     height_shift_range=0.05,
                     shear_range=0.05,
                     zoom_range=0.05,
                     horizontal_flip=True,
                     fill_mode='nearest')
-myGene = trainGenerator(2,'data/membrane/train','image','label',data_gen_args,save_to_dir = None)
+
+run_mode = "train_GPUs"
+save_mode = "single"
+# Choose Devices
+os.environ["CUDA_VISIBLE_DEVICES"] = "1,2,3"
 
 model = SDFCN()
-model_checkpoint = ModelCheckpoint('SDFCN_membrane.hdf5', monitor='loss',verbose=1, save_best_only=True)
-model.fit_generator(myGene,steps_per_epoch=600, epochs=10, callbacks=[model_checkpoint])
-testGene = testGenerator("data/membrane/test")
-results = model.predict_generator(testGene,30,verbose=1)
-saveResult("data/membrane/test",results)
-TensorBoard(log_dir="./logs")
+if run_mode == "train":
+    myGene = trainGenerator(15,'temp_data/pitches/train','image','label',data_gen_args,save_to_dir = None)
+    model_checkpoint = ModelCheckpoint('SDFCN_membrane_0912.hdf5', monitor='loss',verbose=1, save_best_only=False)
+    model.compile(optimizer=Adam(lr=1e-4), loss='categorical_crossentropy', metrics=['accuracy'])
+    model.fit_generator(myGene,steps_per_epoch=100, epochs=1e4,
+                        callbacks=[model_checkpoint,TensorBoard(log_dir="./log_0912")],
+                        shuffle=True,workers=2, use_multiprocessing=True)
+
+elif run_mode == "train_GPUs":
+    myGene = trainGenerator(15, 'temp_data/pitches/train', 'image', 'label', data_gen_args, save_to_dir=None)
+    parallel_model = multi_gpu_model(model, gpus=3)
+    checkpoint = ParallelModelCheckpoint(model, filepath='SDFCN_membrane_0913.hdf5', monitor='loss', verbose=1,
+                                         save_best_only=False)  # 解决多GPU运行下保存模型报错的问题
+    parallel_model.compile(optimizer=Adam(lr=1e-4), loss='categorical_crossentropy', metrics=['accuracy'])
+    parallel_model.fit_generator(myGene,steps_per_epoch=100,epochs=10,
+                                 callbacks=[TensorBoard(log_dir="./log_0913_multgpu")],
+                                 shuffle=True) # workers=2, use_multiprocessing=True
+elif run_mode == "test":
+    model.load_weights("SDFCN_membrane_0912.hdf5")
+
+# pitch数
+num = 1000
+# 融合大图时，每张总共的小pitch
+each_image_size = 1
+# temp_data/pitches/test/image
+testGene = testGenerator("temp_data/temp_test",num * each_image_size)
+results = model.predict_generator(testGene, num * each_image_size, verbose=1)
+
+if save_mode == "single":
+    saveResult("temp_data/temp_test",results)
+elif save_mode == "full":
+    saveBigResult("temp_data/full", results, init_box, each_image_size, num)
--- model.py
+++ model.py
@@ -1,14 +1,10 @@
-import numpy as np 
-import os
-import numpy as np
-from keras.models import *
-from keras.layers import *
-from keras.optimizers import *
-from keras.callbacks import ModelCheckpoint, LearningRateScheduler
-from keras import backend as keras
+# tensorflow.python.keras.
+from tensorflow.python.keras.models import *
+from tensorflow.python.keras.layers import *
+from tensorflow.python.keras.optimizers import *
+import pydensecrf.densecrf as dcrf
 
-
-def SDFCN(pretrained_weights = None,input_size = (256,256,1)):
+def SDFCN(pretrained_weights = None,input_size = (256,256,3)):
     inputs = Input(input_size)
     conv1_1 = Conv2D(64, 3, activation='relu', padding='same')(inputs)
     conv1_2 = Conv2D(64, 3, activation='relu', padding='same')(conv1_1)
@@ -33,14 +29,22 @@ def SDFCN(pretrained_weights = None,input_size = (256,256,1)):
     conv7_2 = Conv2D(64, 3, activation='relu', padding='same')(conv7_1)
     conv7_3 = Conv2D(64, 3, activation='relu', padding='same')(conv7_2)
 
-    # conv8 = Conv2D(1, 1, activation='softmax')(conv7_3)
+    # Softmax模式
+    # conv8_0 = Conv2D(2, 1, strides=1, padding='same')(conv7_3)
+    # conv8 = Activation('softmax')(conv8_0)
+    # model = Model(inputs=inputs, outputs=conv8)
 
-    conv8 = Conv2D(1, 1, activation='sigmoid')(conv7_3)
+    # Sigmoid模式
+    conv8 = Conv2D(2, 1, strides=1, padding='same')(conv7_3)
+    activation = Activation('softmax')(conv8)
 
-    model = Model(inputs=inputs, outputs=conv8)
+    #CRF
+    dcrf.DenseDRF2D(256,256,)
 
-    model.compile(optimizer=Adam(lr=1e-4), loss='binary_crossentropy', metrics=['accuracy'])
+    model = Model(inputs=inputs,outputs=activation)
 
+    # model.compile(optimizer=Adam(lr=1e-4), loss='binary_crossentropy', metrics=['accuracy'])
+    # categorical_crossentropy binary_crossentropy
     if (pretrained_weights):
         model.load_weights(pretrained_weights)
 
@@ -49,40 +53,40 @@ def SDFCN(pretrained_weights = None,input_size = (256,256,1)):
 """
     Old SNFCN
 """
-def SNFCN(pretrained_weights=None, input_size=(256, 256, 1)):
-    inputs = Input(input_size)
-    conv1_1 = Conv2D(64, 3, activation='relu', padding='same')(inputs)
-    conv1_2 = Conv2D(64, 3, activation='relu', padding='same')(conv1_1)
-    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1_2)
-
-    sc2 = shortcutblock(128)(pool1)
-    pool2 = MaxPooling2D(pool_size=(2, 2))(sc2)
-
-    sc3 = shortcutblock(256)(pool2)
-    pool3 = MaxPooling2D(pool_size=(2, 2))(sc3)
-
-    sc4 = shortcutblock(512)(pool3)
-    sc4 = shortcutblock(512)(sc4)
-
-    conv5_1 = Conv2DTranspose(256, 3, strides=2, use_bias=False, padding='same')(sc4)
-    conv5_2 = shortcutblock(256)(conv5_1)
-
-    conv6_1 = Conv2DTranspose(128, 3, strides=2, use_bias=False, padding='same')(conv5_2)
-    conv6_2 = shortcutblock(128)(conv6_1)
-    conv7_1 = Conv2DTranspose(64, 3, strides=2, use_bias=False, padding='same')(conv6_2)
-    conv7_2 = Conv2D(64, 3, activation='relu', padding='same')(conv7_1)
-    conv7_3 = Conv2D(64, 3, activation='relu', padding='same')(conv7_2)
-
-    conv8 = Conv2D(1, 1, activation='softmax')(conv7_3)
-
-    model = Model(inputs=inputs, outputs=conv8)
-
-    model.compile(optimizer=Adam(lr=1e-4), loss='binary_crossentropy', metrics=['accuracy'])
-
-    if (pretrained_weights):
-        model.load_weights(pretrained_weights)
-
-    return model
+# def SNFCN(pretrained_weights=None, input_size=(256, 256, 1)):
+#     inputs = Input(input_size)
+#     conv1_1 = Conv2D(64, 3, activation='relu', padding='same')(inputs)
+#     conv1_2 = Conv2D(64, 3, activation='relu', padding='same')(conv1_1)
+#     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1_2)
+#
+#     sc2 = shortcutblock(128)(pool1)
+#     pool2 = MaxPooling2D(pool_size=(2, 2))(sc2)
+#
+#     sc3 = shortcutblock(256)(pool2)
+#     pool3 = MaxPooling2D(pool_size=(2, 2))(sc3)
+#
+#     sc4 = shortcutblock(512)(pool3)
+#     sc4 = shortcutblock(512)(sc4)
+#
+#     conv5_1 = Conv2DTranspose(256, 3, strides=2, use_bias=False, padding='same')(sc4)
+#     conv5_2 = shortcutblock(256)(conv5_1)
+#
+#     conv6_1 = Conv2DTranspose(128, 3, strides=2, use_bias=False, padding='same')(conv5_2)
+#     conv6_2 = shortcutblock(128)(conv6_1)
+#     conv7_1 = Conv2DTranspose(64, 3, strides=2, use_bias=False, padding='same')(conv6_2)
+#     conv7_2 = Conv2D(64, 3, activation='relu', padding='same')(conv7_1)
+#     conv7_3 = Conv2D(64, 3, activation='relu', padding='same')(conv7_2)
+#
+#     conv8 = Conv2D(1, 1, activation='softmax')(conv7_3)
+#
+#     model = Model(inputs=inputs, outputs=conv8)
+#
+#     model.compile(optimizer=Adam(lr=1e-4), loss='binary_crossentropy', metrics=['accuracy'])
+#
+#     if (pretrained_weights):
+#         model.load_weights(pretrained_weights)
+#
+#     return model
 
 """
     ShortCutBlock Layer
